a72329c5c149fac8fd0588e9fff1bd2fedef44f1
erich <erich@ebremer.com>
2019-07-31 12:17:14 -0400

    add support for different extensions other than vsi

--- run_convert.sh
+++ run_convert.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
-    echo "run_convert.sh <input folder>"
+if [ $# -ne 2 ]; then
+    echo "run_convert.sh <input folder> <file extension>"
     exit 1
 fi
 
 folder=$1
+extension=$2
 if [ ! -d "$folder" ]; then
    echo "Folder: " $inp_folder "does not exist."
    exit 1
 fi
 mkdir -p $folder/converted
 
-find $folder -iname "*.vsi" | while read i
+find $folder -iname "*.$extension" | while read i
 do
 	echo $i;
 	fname="$(basename "$i")"
